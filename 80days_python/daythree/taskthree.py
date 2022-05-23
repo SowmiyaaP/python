68cd05b1c9aceee22baf19d1e1c3c054f8874f5b
SowmiyaaP <89406839+SowmiyaaP@users.noreply.github.com>
2022-05-23 19:58:26 +0530

    Delete taskthree.py

--- 80days_python/daythree/taskthree.py
+++
@@ -1,9 +0,0 @@
-leap= int(input("Enter the year:"))
-if leap % 4 == 0:
-    if leap % 100 != 0:
-       print(f"{leap} is a leap year")
-    elif leap % 400 == 0:
-        print(f"{leap} is a leap year")
-else:
-    print(f"{leap} is not a leap year")
-
